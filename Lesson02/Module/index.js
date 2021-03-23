710d69ecf160bef1a8ef477c6acd3bb5f40dc7fa
frtdkmn <frtdkmn@gmail.com>
2021-03-23 22:55:19 +0300

    Added hello world app codes

--- Lesson02/Module/index.js
+++ Lesson02/Module/index.js
@@ -1,6 +1,7 @@
 //import defFunc , { sum as topla,divNum } from './mathModule'
-import * as mathFunc from './mathModule'
-console.log("Hello!")
-console.log(mathFunc.sum(1,2))
-console.log(mathFunc.divNum(1,2))
-console.log(mathFunc.default(1,2))
+import * as mathFunc from "./mathModule";
+console.log("Hello!");
+console.log(mathFunc.sum(1, 2));
+console.log(mathFunc.divNum(1, 2));
+console.log(mathFunc.default(1, 2));
+
