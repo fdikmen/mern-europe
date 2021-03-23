0ab909259dd80914978369d774a26c2ade2ebec6
frtdkmn <frtdkmn@gmail.com>
2021-03-23 20:58:47 +0300

    Added module codes.

--- Lesson02/Module/index.js
+++ Lesson02/Module/index.js
@@ -1,3 +1,6 @@
-import { sum } from './mathModule'
+//import defFunc , { sum as topla,divNum } from './mathModule'
+import * as mathFunc from './mathModule'
 console.log("Hello!")
-console.log(sum(1,2))
+console.log(mathFunc.sum(1,2))
+console.log(mathFunc.divNum(1,2))
+console.log(mathFunc.default(1,2))
--- Lesson02/Module/mathModule.js
+++ Lesson02/Module/mathModule.js
@@ -1,1 +1,6 @@
 export const sum = (x, y) => x + y;
+export const divNum = (x, y) => x / y;
+
+
+const carpNum = (x, y) => x * y;
+export default carpNum ;
