4a04504f03594c9587a9231390d4716c91acbebf
frtdkmn <frtdkmn@gmail.com>
2021-03-28 19:21:47 +0300

    Props, PropsTypes and Constructor codes addeed.

--- Lesson02/helloworld/src/components/Blog.js
+++ Lesson02/helloworld/src/components/Blog.js
@@ -4,14 +4,27 @@ import BlogItem from "./BlogItem";
 
 //class Blog extends React.Component {}
 class Blog extends Component {
-  render() {
+   render() {
     let blogText = "TEXT TEXT TEXT";
     const blogParaf =<h2>BLOG NOTES <button>BUTTON</button></h2>
     let nowNumber = 123;
+    let blogInfos = [{textHead:"Blog 1",textDesc:"Blog 1 Description ...."},
+    {textHead:"Blog 2",textDesc:"Blog 2 Description ...."},
+    {textHead:"Blog 3",textDesc:"Blog 3 Description ...."}
+  ,{textHead:"Blog 4",textDesc:"Blog 4 Description ...."}]
     return (
       <div className="blogTitle">
         <h1>BLOG COMPONENT</h1>
-        <BlogItem dataXYZ="new h3 text" newData={blogParaf} nowNmbr={nowNumber} blogMessage="Testing with PRPS" />
+        {/* <BlogItem title={blogInfos[0].textHead} description={blogInfos[0].textDesc}/>
+        <BlogItem title={blogInfos[1].textHead} description={blogInfos[1].textDesc}/>
+        <BlogItem title={blogInfos[2].textHead} description={blogInfos[2].textDesc}/>
+        <BlogItem title={blogInfos[3].textHead} description={blogInfos[3].textDesc}/> */}
+        
+        {blogInfos.map((currentValue,index)=> 
+        <BlogItem key={index} title={currentValue.textHead} description={currentValue.textDesc}/>)}
+
+
+        <BlogItem title="Student" dataXYZ="new h3 text" newData={blogParaf} nowNmbr={nowNumber} blogMessage="Testing with PRPS" />
         <p>{blogText}</p>
         <label>{5 / 2 === 2.5 ? "YES" : "NO"}</label>
         {blogParaf}
--- Lesson02/helloworld/src/components/BlogItem.js
+++ Lesson02/helloworld/src/components/BlogItem.js
@@ -1,18 +1,48 @@
 //CREATE WITH rcc SNIPPET
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class BlogItem extends Component {
-    render() {
+    constructor(props){
+        super(props);
+        console.log(props)
         console.log(this.props)
-        const {blogMessage,nowNmbr} =this.props
-        return (
-            <div>
-                <h3>{this.props.dataXYZ}</h3>
-                {/* <h4>{this.props.blogMessage}</h4>
+
+        
+      }
+  /**
+   * user={}
+   * user={age:12}
+   * user={name:"Tommy",age:12}
+   */
+  
+    static propTypes = {
+    title: PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired,
+    description: PropTypes.string,
+    blogMessage:PropTypes.string,
+    nowNmbr:PropTypes.any,
+    dataXYZ:PropTypes.string
+   // user:PropTypes.shape({name:PropTypes.string,age:PropTypes.number.isRequired}).isRequired
+  };
+
+  //Default Props
+  static defaultProps = {description:"One Desc",title:"One Title"}
+
+
+  render() {
+    //console.log(this.props);
+    const { blogMessage, nowNmbr } = this.props;
+    return (
+      <div>
+        
+        <h3>{this.props.dataXYZ}</h3>
+        {/* <h4>{this.props.blogMessage}</h4>
                 <h4>{this.props.nowNmbr}</h4> */}
-                <h4>{blogMessage}</h4>
-                <h4>{nowNmbr}</h4>
-            </div>
-        )
-    }
+        <h4>{blogMessage}</h4>
+        <h4>{nowNmbr}</h4>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+      </div>
+    );
+  }
 }
