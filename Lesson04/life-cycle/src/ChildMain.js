0a58370e0804641b4535b794f989d574283460d1
frtdkmn <frtdkmn@gmail.com>
2021-04-07 20:44:22 +0300

    Added new life-cycle codes.

--- Lesson04/life-cycle/src/ChildMain.js
+++ Lesson04/life-cycle/src/ChildMain.js
@@ -11,14 +11,37 @@ constructor(props) {super(props)
 componentWillMount() {console.log("Child componentWillMount runned.")}
  //created by "cwr" snippet
  componentWillReceiveProps(nextProps) {
-    console.log("ComponentWillReceiveProps Runned.nextProps:",nextProps)
+    console.log("Child ComponentWillReceiveProps Runned.nextProps:",nextProps)
     }
+//created by "SCU" snippet
+shouldComponentUpdate(nextProps, nextState) {
+    console.log("Child ShouldComponentUpdate Runned. nextProps:",nextProps
+    ,"nextState:",nextState)
+    if (nextState.number > 50) {
+        return true;
+    } else {
+        return false;
+    }
+   
+}
+//created by "CWUP" snippet
+componentWillUpdate(nextProps, nextState) {
+    console.log("Child ComponentWillUpdate Runned. nextProps:",nextProps
+    ,"nextState:",nextState)
+}
+
+
+state={number:0}
+changeNumber=(event)=>{this.setState({number:event.target.value})}
+
     render() {
         console.log("Child render runned.")
         return (
             <div>
                 ChildMain Comp...
                 <h2>Props: {this.props.name}</h2>
+                <input name="name" id="name" onChange={this.changeNumber}></input>
+                <br/>Number: {this.state.number} /10= {this.state.number/10}
             </div>
         )
     }
