fa091379976fd00c3a0e4ba873f92692216053bb
frtdkmn <frtdkmn@gmail.com>
2021-05-02 17:07:43 +0300

    Added first redux codes.

--- Lesson07/first-redux-app/src/App.js
+++ Lesson07/first-redux-app/src/App.js
@@ -8,20 +8,15 @@ function reducer(state,action)
   {
     return action.payload.newState;
   }
-  else if(action.type === 'deposit'){
-    return action.payload;
-  }
   return "StateXYZ";}
 
 const myStore = createStore(reducer);
 console.log("(OLD)myStore.getState()=>",myStore.getState());
 
 const action = {type:'changeTheState',payload:{newState:'My new state'}}
-const actionDeposit = {type:'deposit',payload:10}
 
 myStore.dispatch(action);
-console.log("(NEW)myStore.getState()=>",myStore.getState());
-myStore.dispatch(actionDeposit);
+
 console.log("(NEW)myStore.getState()=>",myStore.getState());
 
 function App() {
