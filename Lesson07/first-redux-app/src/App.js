1f1857eaa6c42f50f0b57339cd78985e239c688e
frtdkmn <frtdkmn@gmail.com>
2021-05-02 18:22:16 +0300

    Added Redux-2 codes.

--- Lesson07/first-redux-app/src/App.js
+++ Lesson07/first-redux-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-  import {createStore} from 'redux'
+  import {createStore,combineReducers} from 'redux'
 
 function reducer(state,action)
 {
@@ -10,14 +10,39 @@ function reducer(state,action)
   }
   return "StateXYZ";}
 
-const myStore = createStore(reducer);
+function userReducer(state ='' ,action){
+  return state;
+}
+
+function productReducer(state = [],action){
+  return state;
+}
+
+const rootReducer = combineReducers({ userReducer,productReducer})
+
+//const myStore = createStore(reducer);
+const myStore = createStore(rootReducer,
+  {
+    userReducer:'Tommy',
+    productReducer:[{name:'Sony',type:"Music Player"}]
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 console.log("(OLD)myStore.getState()=>",myStore.getState());
+
+myStore.subscribe(()=>{
+  ///alert("STORE CHANGED BRO!!!");
+  //console.log("Store updated. New state=",myStore.getState());
+})
+
 
 const action = {type:'changeTheState',payload:{newState:'My new state'}}
 
 myStore.dispatch(action);
 
-console.log("(NEW)myStore.getState()=>",myStore.getState());
+//console.log("(NEW)myStore.getState()=>",myStore.getState());
+
+
+
 
 function App() {
   return (
