c5d5cade2e81c88b14c0214b6cf170638e33e34f
frtdkmn <frtdkmn@gmail.com>
2021-05-19 20:21:57 +0300

    Added App.js codes.

--- Lesson09-FinalProject/movie-app/src/App.js
+++ Lesson09-FinalProject/movie-app/src/App.js
@@ -11,8 +11,10 @@ import {  Container,  Divider,  Dropdown,  Grid,  Header,  Icon,  Image,
     borderRadius: 0,
     boxShadow: 'none',
     marginBottom: '1em',
-    marginTop: '4em',
+    marginTop: '1em',
     transition: 'box-shadow 0.5s ease, padding 0.5s ease',
+    // backgroundColor:'#F6F6F6',
+    borderBottom: '1px solid #f6f6f6'
   }
   
   const fixedMenuStyle = {
@@ -20,63 +22,7 @@ import {  Container,  Divider,  Dropdown,  Grid,  Header,  Icon,  Image,
     border: '1px solid #ddd',
     boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
   }
-  
-  const overlayStyle = {
-    float: 'left',
-    margin: '0em 3em 1em 0em',
-  }
-  
-  const fixedOverlayStyle = {
-    ...overlayStyle,
-    position: 'fixed',
-    top: '80px',
-    zIndex: 10,
-  }
-  
-  const overlayMenuStyle = {
-    position: 'relative',
-    left: 0,
-    transition: 'left 0.5s ease',
-  }
-  
-  const fixedOverlayMenuStyle = {
-    ...overlayMenuStyle,
-    left: '800px',
-  }
-  
-  const LeftImage = () => (
-    <Image
-      floated='left'
-      size='medium'
-      src='/images/wireframe/square-image.png'
-      style={{ margin: '2em 2em 2em -4em' }}
-    />
-  )
-  
-  const RightImage = () => (
-    <Image
-      floated='right'
-      size='medium'
-      src='/images/wireframe/square-image.png'
-      style={{ margin: '2em -4em 2em 2em' }}
-    />
-  )
-  
-  const Paragraph = () => (
-    <p>
-      {[
-        'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ',
-        'tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas ',
-        'semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ',
-        'ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean ',
-        'fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. ',
-        'Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor ',
-        'neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, ',
-        'accumsan porttitor, facilisis luctus, metus',
-      ].join('')}
-    </p>
-  )
-  
+   
 
 export class App extends Component {
   state = {
@@ -84,9 +30,6 @@ export class App extends Component {
     overlayFixed: false,
   }
 
-  handleOverlayRef = (c) => {
-    const { overlayRect } = this.state   
-  }
 
   stickOverlay = () => this.setState({ overlayFixed: true })
 
@@ -97,22 +40,11 @@ export class App extends Component {
   unStickTopMenu = () => this.setState({ menuFixed: false })
   
   render() {
-    const { menuFixed, overlayFixed, overlayRect } = this.state
+    const { menuFixed} = this.state
     return (
      
       <div>
-        {/* Heads up, style below isn't necessary for correct work of example, simply our docs defines other
-            background color.
-          */}
-        <style>
-          {`
-          html, body {
-            background: #fff;
-          }
-        `}
-        </style>
-
-        
+               
         <Visibility
           onBottomPassed={this.stickTopMenu}
           onBottomVisible={this.unStickTopMenu}
@@ -129,27 +61,7 @@ export class App extends Component {
               </Menu.Item>
               <Menu.Item header>Project Name</Menu.Item>
               <Menu.Item as='a'>Blog</Menu.Item>
-              <Menu.Item as='a'>Articles</Menu.Item>
-
-              <Menu.Menu position='right'>
-                <Dropdown text='Dropdown' pointing className='link item'>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Header Item</Dropdown.Header>
-                    <Dropdown.Item>
-                      <i className='dropdown icon' />
-                      <span className='text'>Submenu</span>
-                      <Dropdown.Menu>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Menu.Menu>
+              <Menu.Item as='a'>Articles</Menu.Item>            
             </Container>
           </Menu>
         </Visibility>
