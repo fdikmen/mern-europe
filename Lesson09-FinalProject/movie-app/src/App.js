11d50d3d69619b30d3cf0ac13c54c5ea64fe0a81
frtdkmn <frtdkmn@gmail.com>
2021-05-19 20:56:02 +0300

    Added Header.js and Footer.js

--- Lesson09-FinalProject/movie-app/src/App.js
+++ Lesson09-FinalProject/movie-app/src/App.js
@@ -1,143 +1,26 @@
 //Created by [rce] snippet.
-import React, { Component } from 'react'
-import './App.css';
-import { Link,Route } from 'react-router-dom';
-import MoviePage from './components/pages/MoviePage'
-import {  Container,  Divider,  Dropdown,  Grid,  Header,  Icon,  Image,
-  List,  Menu,  Segment,  Visibility,} from "semantic-ui-react";
-
-  const menuStyle = {
-    border: 'none',
-    borderRadius: 0,
-    boxShadow: 'none',
-    marginBottom: '1em',
-    marginTop: '1em',
-    transition: 'box-shadow 0.5s ease, padding 0.5s ease',
-    // backgroundColor:'#F6F6F6',
-    borderBottom: '1px solid #f6f6f6'
-  }
-  
-  const fixedMenuStyle = {
-    backgroundColor: '#fff',
-    border: '1px solid #ddd',
-    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-  }
-   
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import MoviePage from "./components/pages/MoviePage";
+import { Container } from "semantic-ui-react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 export class App extends Component {
-  state = {
-    menuFixed: false,
-    overlayFixed: false,
-  }
-
-
-  stickOverlay = () => this.setState({ overlayFixed: true })
-
-  stickTopMenu = () => this.setState({ menuFixed: true })
-
-  unStickOverlay = () => this.setState({ overlayFixed: false })
-
-  unStickTopMenu = () => this.setState({ menuFixed: false })
-  
   render() {
-    const { menuFixed} = this.state
     return (
-     
       <div>
-               
-        <Visibility
-          onBottomPassed={this.stickTopMenu}
-          onBottomVisible={this.unStickTopMenu}
-          once={false}
-        >
-          <Menu
-            borderless
-            fixed={menuFixed ? 'top' : undefined}
-            style={menuFixed ? fixedMenuStyle : menuStyle}
-          >
-            <Container text>
-              <Menu.Item>
-                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
-              </Menu.Item>
-              <Menu.Item header>Project Name</Menu.Item>
-              <Menu.Item as='a'>Blog</Menu.Item>
-              <Menu.Item as='a'>Articles</Menu.Item>            
-            </Container>
-          </Menu>
-        </Visibility>
-    
+        <Header />
         <Container text>
-         
-              Hello world!
-              <br/>
-              <Link to="movies">Movies</Link>
-              <Route path="/movies" component={MoviePage}></Route>
-    </Container>
-
-        <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
-          <Container textAlign='center'>
-            <Grid columns={4} divided stackable inverted>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 1' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Footer Header' />
-                  <p>
-                    Extra space for a call to action inside the footer that could help re-engage
-                    users.
-                  </p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Divider inverted section />
-            <Image src='https://react.semantic-ui.com/logo.png' centered size='mini' />
-            <List horizontal inverted divided link size='small'>
-              <List.Item as='a' href='#'>
-                Site Map
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Contact Us
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Terms and Conditions
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
+          Hello world!
+          <br />
+          <Route path="/movies" component={MoviePage}></Route>
+        </Container>
+        <Footer />
       </div>
-
-
-
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ Lesson09-FinalProject/movie-app/src/helpers/styleHelpers.js
@@ -0,0 +1,17 @@
+
+  export const menuStyle = {
+    border: 'none',
+    borderRadius: 0,
+    boxShadow: 'none',
+    marginBottom: '1em',
+    marginTop: '1em',
+    transition: 'box-shadow 0.5s ease, padding 0.5s ease',
+    // backgroundColor:'#F6F6F6',
+    borderBottom: '1px solid #f6f6f6'
+  }
+  
+  export const fixedMenuStyle = {
+    backgroundColor: '#fff',
+    border: '1px solid #ddd',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+  }
