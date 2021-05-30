9702ece5fc629ec80f0e6e0707c5f9798c068d73
frtdkmn <frtdkmn@gmail.com>
2021-05-30 18:07:16 +0300

    Added homepage & surge codes.

--- Lesson09-FinalProject/movie-app/src/App.js
+++ Lesson09-FinalProject/movie-app/src/App.js
@@ -7,6 +7,7 @@ import { Container } from "semantic-ui-react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NewMoviePage from "./components/pages/NewMoviePage"
+import HomePage from "./components/pages/HomePage";
 
 export class App extends Component {
   render() {
@@ -14,8 +15,7 @@ export class App extends Component {
       <div>
         <Header />
         <Container text>
-          Hello world!
-          <br />
+          <Route exact path="/" component={HomePage}></Route>          
           <Route exact path="/movies" component={MoviePage}></Route>          
           <Route exact path="/movies/new" component={NewMoviePage}></Route>
           <Route exact path="/movie/:id" component={NewMoviePage}></Route>
