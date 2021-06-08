06b388410655fa973c7d0fff399ff1715f2e5124
frtdkmn <frtdkmn@gmail.com>
2021-06-08 19:59:38 +0300

    Added new pug files.

--- Lesson12/pug-sample/index.js
+++ Lesson12/pug-sample/index.js
@@ -1,11 +1,14 @@
+//Created by node-snippet
 const express = require('express')
 const app=express()
 const port=3000
 
+//Enable PUG
 app.set('view engine','pug')
 
 app.get('/',(req,res)=>res.send("Hello world from express."))
 
-app.get('/hello',(req,res)=>res.render('index'))
+app.get('/hello',(req,res)=>res.render('index.pug',
+                            {name:"Priscilla",phone:"1-482-255-4328 x631"}))
 
 app.listen(port)
