be7a253b2774fd3945f621265edfeaa56c3feec3
frtdkmn <frtdkmn@gmail.com>
2021-06-16 22:01:14 +0300

    Added mongoose queries.

--- Lesson14/mongoose-sample/models/Post.js
+++ Lesson14/mongoose-sample/models/Post.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema
 const PostSchema = new Schema(
     {
         userId:Number,
-        id:Number,
-        title:String,
-        body:String
+        id:{required:true,unique:true,type:Number},
+        title:{type:String,required:true},
+        body:String,
+        isActive:{type:Boolean,default:true},
+        meta:{votes:Number,favs:Number},
+        comments:[{message:String}],
+        createdDate:{type:Date,default:Date.now}
 
     })
 
--- Lesson14/mongoose-sample/routes/post.Route.js
+++ Lesson14/mongoose-sample/routes/post.Route.js
@@ -1,17 +1,70 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
-const Post = require('../models/Post')
+const PostModel = require("../models/Post");
 
-router.get('/', (req, res) => {
-    const _post = new Post({
-        title:'My new title two',id:2,body:'Body Content'
-    })
-    _post.save((err,data)=>{
-            if(err){console.log(err)}
-            res.json(data)
-    })
-  //res.send('GET request to the homepage')
+router.get('/search', (req, res) => {
+   /* PostModel.find({isActive:false,title: "Test Title"})
+                .then((resultData)=>{res.json(resultData)})
+                .catch((errorDetail)=>{res.json(errorDetail)}) */
+
+    PostModel.find({ isActive: false }, (errorDetail, resultData) => {
+      if (errorDetail) {res.json(errorDetail);}
+      res.json(resultData);
+    });         
 })
+
+router.get('/searchBy', (req, res) => {
+ 
+     PostModel.findOne({ isActive: false },'isActive title', (errorDetail, resultData) => {
+       if (errorDetail) {res.json(errorDetail);}
+       res.json(resultData);
+     });         
+ })
+
+ router.get('/searchById', (req, res) => {
+ 
+    PostModel.findById('60ca40dbbac9cb3bf45abcac','isActive title', (errorDetail, resultData) => {
+      if (errorDetail) {res.json(errorDetail);}
+      res.json(resultData);
+    });         
+})
+
+router.post("/", (req, res) => {
+    /*const _post = new PostModel({
+       title: req.body.title,
+      id:parseInt(req.body.id),
+      body: req.body.body,
+      isActive: req.body.isActive,
+      comments: req.body.comments,
+      meta: req.body.meta,
+    });*/
+    const _post = new PostModel(req.body);
+    _post.save((err, data) => {
+      if (err) {
+          res.json(err);
+      }
+      res.json(data);
+    });
+  });
+  
+
+router.get("/", (req, res) => {
+  const _post = new PostModel({
+     title: "Test Title",
+    id: 11,
+    body: "Body Content Required",
+    isActive: false,
+    comments: [{ message: "Good Job." }, { message: "Bad Job." }],
+    meta: { votes: 85, favs: 55 },
+  });
+  _post.save((err, data) => {
+    if (err) {
+        res.json(err);
+    }
+    res.json(data);
+  });
+  //res.send('GET request to the homepage')
+});
 
 module.exports = router;
